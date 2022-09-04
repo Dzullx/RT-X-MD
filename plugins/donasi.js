01c2a6a0e649153d14b5fa7579ce57f41d57c9d5
Dzullx <96458245+Dzullx@users.noreply.github.com>
2022-09-05 02:11:53 +0900

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Pulsa [082221792667]
-│ • Dana [082221792667]
+│ • Pulsa [082199086847]
+│ • Dana [082199086847]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • Dana [082221792667]
-│ • Pulsa [082221792667]
+│ • Dana [082199086847]
+│ • Pulsa [082199086847]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
